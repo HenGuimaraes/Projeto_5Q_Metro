31ff7b5c942db225568d4fada6487eef30993010
shinn666 <you@example.com>
2018-05-12 16:46:53 -0300

    site com o select porem grafico não está funcionando

---
+++ site/site/site/pagina1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace site
+{
+    public partial class pagina1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public int PegarTemperatura()//isso é um metodo que retornar INT
+        {
+           /* using (SqlConnection conn = new SqlConnection("Server=tcp:tab132.database.windows.net,1433;Initial Catalog=esporte;Persist Security Info=False;User ID=mateus383@tab132;Password=123456sS;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand("SELECT temperatura FROM Temp WHERE codTemp = (SELECT MAX(codTemp) FROM Temp)", conn))
+                {
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read() == true)
+                        {
+                            return int.Parse(dr["temperatura"].ToString()); // retorna a temperatura que esta no banco
+                        }
+                        else
+                        {
+                            return 0; // se não existir nenhuma temperatura no banco ele retornar 0 (****pode ser qualquer numero aqui)
+                        }
+                    }
+
+                }
+            }
+            */
+            return new Random().Next(30);//gera "temperaturas" aleatorias
+
+
+        }
+    }
+}
