349037d93fd06a7e65d51c22238568678bbc65f4
HenGuimaraes <henri.guimaraes2015@gmail.com>
2018-05-20 15:57:14 -0300

    arrumado

---
+++ site/pagina1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Services;
+
+namespace site
+{
+    public partial class pagina1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        [WebMethod]
+        public static double TemperaturaAtual()
+        {/*
+            using (SqlConnection conn = new SqlConnection("Server=tcp:tab132.database.windows.net,1433;Initial Catalog=esporte;Persist Security Info=False;User ID=mateus383;Password=123456sS;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand("SELECT valor FROM temperatura2 WHERE codTemperatura = (SELECT MAX(codTemperatura) FROM temperatura2)", conn))
+                {
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read() == true)
+                        {
+                            return double.Parse(dr["valor"].ToString());
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+
+                }
+
+            }*/
+            return new Random().Next(30);
+
+
+        }
+
+    }
+
+}
