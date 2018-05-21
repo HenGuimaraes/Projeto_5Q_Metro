3cd148acb1e0f58d695478d08dd66be4f9a31129
Aluno <Aluno@Digital.net>
2018-05-21 18:00:29 -0300

    tentando mexer no header

--- site/site/Home.aspx.designer.cs
+++ site/site/Home.aspx.designer.cs
@@ -11,41 +11,5 @@ namespace site {
     
     
     public partial class Home {
-        
-        /// <summary>
-        /// Controle form1.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// Controle btnUm.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnUm;
-        
-        /// <summary>
-        /// Controle btnDois.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnDois;
-        
-        /// <summary>
-        /// Controle btnTres.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnTres;
     }
 }
