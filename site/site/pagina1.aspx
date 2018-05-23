a0d95c3565d9d1f0a58c76d383352ca5566e99a9
HenGuimaraes <henrique.guimaraes2015@gmail.com>
2018-05-22 22:32:29 -0300

    arrumei a branch

---
+++ site/site/pagina1.aspx
@@ -0,0 +1,52 @@
+﻿    <%@ Page Language="C#" AutoEventWireup="true" CodeBehind="pagina1.aspx.cs" Inherits="site.pagina1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+  
+</head>
+<body>  
+    <script src="http://code.jquery.com/jquery-1.8.2.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        google.charts.load('current', { packages: ['corechart', 'line'] });
+        google.charts.setOnLoadCallback(desenharGrafico);
+
+        var total = 0, grafico = null, data = null;
+
+        function desenharGrafico() {
+            if (data == null) {
+                data = new google.visualization.DataTable();
+                data.addColumn('number', 'Tempo');
+                data.addColumn('number', 'Temperatura ºC');
+
+                grafico = new google.visualization.LineChart(document.getElementById('chartdiv'));
+            }
+
+            grafico.draw(data, { title: "Temperaturas em Tempo Real" });
+
+            setTimeout(function () {
+                $.ajax({
+                    type: 'POST',
+                    dataType: 'json',
+                    contentType: 'application/json',
+                    url: 'pagina1.aspx/TemperaturaAtual',
+                    data: '{}',
+                    success: function (response) {
+                        data.addRow([total, response.d]);
+                        total++;
+                        desenharGrafico();
+                    },
+                    error: function () {
+                    }
+                });
+            }, 1000);
+        }
+
+    </script>
+    <div id="chartdiv"></div>
+</body>
+</html>
---
+++ site/site/registro.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace site
+{
+    public partial class Registro : System.Web.UI.Page
+    {
+        //======================================================================================//
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnlabel.Visible = false;
+            Label2.Visible = false;
+
+            // aqui voce apaga as mensagens de erros
+        }
+        //======================================================================================//
+        protected void btnConfirmar_Click(object sender, EventArgs e)
+        {
+
+            if (btnSenha.Text != btnSenha1.Text)
+            {
+                btnlabel.Visible = true;
+                return;
+            }
+            // confirma se as senhas batem
+            if (string.IsNullOrWhiteSpace(btnnome.Text) || string.IsNullOrWhiteSpace(btnSenha.Text)
+                || string.IsNullOrWhiteSpace(btnSenha1.Text) || (DropDownList1.SelectedValue == "0")
+                || string.IsNullOrWhiteSpace(login.Text))
+            {
+                Label2.Visible = true;
+                return;
+            }// obriga o usuario a preencher todos os espaços
+
+
+
+            using (SqlConnection conn = new SqlConnection("Server=tcp:tab132.database.windows.net,1433;Initial Catalog=esporte;Persist Security Info=False;User ID=mateus383@tab132;Password=123456sS;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand("SELECT login from usuario", conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        //Obtém os registros, um por vez    
+                        while (reader.Read() == true)
+                        {
+
+                            string validacao;
+                            validacao = reader.GetString(0);
+
+                            if (validacao == login.Text)
+                            {
+
+                                login.Text = "o login ja existe tente outro";
+                                return;
+                            }// aqui valida se o vagao ja tem nomeação igual
+
+                        }
+                    }
+                }
+
+
+                //=====================================================================================//
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO usuario VALUES (@login, @senha, @Nome, @cargo)", conn))
+                {
+                    cmd.Parameters.AddWithValue("@login", login.Text);
+                    cmd.Parameters.AddWithValue("@senha", btnSenha.Text);
+                    cmd.Parameters.AddWithValue("@Nome", btnnome.Text);
+
+                    cmd.Parameters.AddWithValue("@cargo", DropDownList1.SelectedValue);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            // aqui é o codigo pra integrar o registro com o banco de dados
+
+        }
+    }
+}
