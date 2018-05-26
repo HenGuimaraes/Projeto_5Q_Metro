9d84ad7db6aa92cdc120c2d4c20738a202b8a6b7
mateus <mateustoralli@hotmail.com>
2018-05-26 13:53:51 -0300

    site2.0

---
+++ site2.0/site/tabela.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="tabela.aspx.cs" Inherits="site.tabela" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="stylesheet" href="css/tabela.css" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="meio">
+           <div class="tremeselect"> 
+           <asp:DropDownList placeholder="trens"  ID="trens" runat="server">
+                     <asp:ListItem text="trens" value="0"></asp:ListItem>
+                  </asp:DropDownList>  
+           <asp:Button Text="atualizar" ID="att" runat="server" onclick="att_Click" />
+               </div>
+             <div class="vagau">  <asp:DropDownList placeholder="vagoes" id="vagau" runat="server"> 
+               
+           </asp:DropDownList> </div>
+               <div class="vagao">
+            <asp:button id="vagao1" runat="server" ReadOnly="True" Visible="False"></asp:button>
+            <asp:button id="vagao2" runat="server" ReadOnly="True" Visible="False"></asp:button>
+            <asp:button id="vagao3" runat="server" ReadOnly="True" Visible="False"></asp:button>
+            <asp:button id="vagao4" runat="server" ReadOnly="True" Visible="False"></asp:button>
+            <asp:button id="vagao5" runat="server" ReadOnly="True" Visible="False"></asp:button>
+            <asp:button id="vagao6" runat="server" ReadOnly="True" Visible="False"></asp:button>
+            <asp:button id="vagao7" runat="server" ReadOnly="True" Visible="False"></asp:button>
+            <asp:button id="vagao8" runat="server" ReadOnly="True" Visible="False"></asp:button>
+                   </div>
+               <div class="label">
+               <asp:Label ID="label1" runat="server" text=""></asp:Label>
+               <asp:Label ID="label2" runat="server" text=""></asp:Label>
+               <asp:Label ID="label3" runat="server" text=""></asp:Label>
+               <asp:Label ID="label4" runat="server" text=""></asp:Label>
+               <asp:Label ID="label5" runat="server" text=""></asp:Label>
+               <asp:Label ID="label6" runat="server" text=""></asp:Label>
+               <asp:Label ID="label7" runat="server" text=""></asp:Label>
+               <asp:Label ID="label8" runat="server" text=""></asp:Label>
+                   </div>             
+        </div>
+    </form>
+</body>
+</html>
---
+++ site2.0/site/tabela.aspx.designer.cs
@@ -0,0 +1,195 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace site {
+    
+    
+    public partial class tabela {
+        
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Controle trens.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList trens;
+        
+        /// <summary>
+        /// Controle att.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button att;
+        
+        /// <summary>
+        /// Controle vagau.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList vagau;
+        
+        /// <summary>
+        /// Controle vagao1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button vagao1;
+        
+        /// <summary>
+        /// Controle vagao2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button vagao2;
+        
+        /// <summary>
+        /// Controle vagao3.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button vagao3;
+        
+        /// <summary>
+        /// Controle vagao4.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button vagao4;
+        
+        /// <summary>
+        /// Controle vagao5.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button vagao5;
+        
+        /// <summary>
+        /// Controle vagao6.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button vagao6;
+        
+        /// <summary>
+        /// Controle vagao7.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button vagao7;
+        
+        /// <summary>
+        /// Controle vagao8.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button vagao8;
+        
+        /// <summary>
+        /// Controle label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label1;
+        
+        /// <summary>
+        /// Controle label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label2;
+        
+        /// <summary>
+        /// Controle label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label3;
+        
+        /// <summary>
+        /// Controle label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label4;
+        
+        /// <summary>
+        /// Controle label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label5;
+        
+        /// <summary>
+        /// Controle label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label6;
+        
+        /// <summary>
+        /// Controle label7.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label7;
+        
+        /// <summary>
+        /// Controle label8.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label8;
+    }
+}
